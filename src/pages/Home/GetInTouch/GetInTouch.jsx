6c5003fa08ea2dbd872f1409ddba9068efd3e92b
shafaet <lbshaft01@gmail.com>
2023-05-21 23:55:56 +0600

    smal fiex

--- src/pages/Home/GetInTouch/GetInTouch.jsx
+++ src/pages/Home/GetInTouch/GetInTouch.jsx
@@ -4,7 +4,7 @@ import picture from "../../../assets/Get.png";
 const GetInTouch = () => {
   return (
     <section className="container mx-auto my-10 overflow-hidden">
-        <h2 className=" lg:text-5xl font-extrabold text-center">Get in Touch</h2>
+      <h2 className=" lg:text-5xl font-extrabold text-center">Get in Touch</h2>
       <div className="hero my-9 container mx-auto">
         <div className="hero-content flex-col lg:flex-row-reverse gap-9">
           <div className="text-center lg:text-left" data-aos="fade-left">
@@ -40,7 +40,7 @@ const GetInTouch = () => {
                 </div>
                 <div className="form-control mt-6">
                   <button type="submit" className="btn btn-secondary">
-                    Login
+                    send
                   </button>
                 </div>
                 <div></div>
